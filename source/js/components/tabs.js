e3f2aa1c3d2f90ea393e966dc131153ce47f7072
roman <romasaab@gmail.com>
2024-04-29 15:25:35 +0300

    add scroll tabs-nav

--- source/js/components/tabs.js
+++ source/js/components/tabs.js
@@ -5,9 +5,9 @@ import {
 } from "../functions/customFunctions";
 import vars from "../_vars";
 
-const areasTab = document.querySelectorAll('.areas-tabs')
-const firstCount = document.querySelector('[data-count]');
-const count = document.querySelector('[data-count-join]');
+const areasTab = document.querySelectorAll(".areas-tabs");
+const firstCount = document.querySelector("[data-count]");
+const count = document.querySelector("[data-count-join]");
 
 // --------------- tabs custom function --------------- //
 const tabsFunction = function (
@@ -40,12 +40,62 @@ const tabsFunction = function (
               ),
               active
             );
-
-  
           });
         });
       }
     });
 };
 
-tabsFunction(document.querySelectorAll(".about-tab"), "data-tab", "data-tab-content");
+tabsFunction(
+  document.querySelectorAll(".about-tab"),
+  "data-tab",
+  "data-tab-content"
+);
+
+const container = document.getElementById("scroll-container");
+
+
+function tabsNavInit(){
+
+  if (window.innerWidth >= 576) {
+    console.log('123');
+    let isMouseDragging = false;
+    let isMouseMove = false;
+
+    if (container) {
+      let startX;
+      let scrollLeft;
+
+      container.addEventListener("mousedown", (e) => {
+        isMouseDragging = true;
+        startX = e.pageX - container.offsetLeft;
+        scrollLeft = container.scrollLeft;
+      });
+
+      container.addEventListener("mouseup", () => {
+        if (!isMouseMove) {
+          const aboutTabElement = document.querySelector(".about-section");
+          if (aboutTabElement) {
+            aboutTabElement.scrollIntoView({
+              behavior: "smooth",
+              block: "start",
+            });
+          }
+        }
+        isMouseDragging = false;
+        isMouseMove = false;
+      });
+
+      container.addEventListener("mousemove", (e) => {
+        if (!isMouseDragging) return;
+        e.preventDefault();
+        const x = e.pageX - container.offsetLeft;
+        const walk = (x - startX) * 2;
+        container.scrollLeft = scrollLeft - walk;
+        isMouseMove = true;
+      });
+    }
+  }
+}
+tabsNavInit();
+window.addEventListener('resize', tabsNavInit);
